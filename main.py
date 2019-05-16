87ef8cb0c33f0d512d3d35203763117770072165
David Butler <dbutler4@hawk.iit.edu>
2019-05-16 13:14:11 -0500

    Got main to successfully call and update label from result of simulation

--- main.py
+++ main.py
@@ -12,12 +12,7 @@ output.set("Returned Value: N/A")
 def callback(event):
     webbrowser.open_new(r"https://www.youtube.com/watch?v=OeJobV4jJG0")
 
-def run():
-    min = minimum.get()
-    max = maximum.get()
-    size = val_size.get()
-    #returnedVal.text =eulerGuess(min, max, size)
-    output.set("Returned Value: SUCCESS!!")
+
 
 description = Label(window, text = "This is a program that simulates the Euler process for guessing the highest number in a series of hidden random numbers.", font = default_font)
 link = Label(window,fg="blue", text = "Link to Vsauce video", cursor = "hand2", font = default_font)
@@ -28,10 +23,6 @@ description.grid(column=0, row=0)
 link.grid(column=0, row =1)
 returnedVal.grid(column= 0, row = 2)
 
-
-start = Button(window, text = "Start", font = default_font, command = "run")
-start.grid(column=0, row = 10)
-
 minimum = Entry(window, width=50)
 maximum = Entry(window, width=50)
 size = Entry(window, width=50)
@@ -39,6 +30,18 @@ size = Entry(window, width=50)
 minimum.grid(column=0, row = 3)
 maximum.grid(column=0, row = 4)
 size.grid(column=0, row = 5)
+
+def run():
+    min = minimum.get()
+    max = maximum.get()
+    valSize = size.get()
+    #returnedVal.text =eulerGuess(min, max, size)
+    output.set("Returned Value: " + str(eulerGuess(0,0,0)))
+
+start = Button(window, text = "Start", font = default_font, command = run)
+start.grid(column=0, row = 10)
+
+
 
 
 window.mainloop()
