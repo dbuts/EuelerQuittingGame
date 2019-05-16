13a580ada81a49c2915e92cb2de55405c2413592
David Butler <dbutler4@hawk.iit.edu>
2019-05-16 16:38:52 -0500

    Further updated formatting and added some commenting

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ from tkinter import *
 from simulation import *
 import webbrowser
 
+#Setup the GUI itsef
 window = Tk()
 window.title("Euler's Guessing Simulator")
-window.geometry('1600x900')
+window.geometry('1920x1080')
 window.columnconfigure(0, weight = 1)
 window.columnconfigure(1, weight = 4)
 
 default_font = ("Arial", 12)
+
+#Variable to control output label's text
 output = StringVar()
 output.set("Returned Value: N/A")
 
+#Functions
 def callback(event):
     webbrowser.open_new(r"https://www.youtube.com/watch?v=OeJobV4jJG0")
 
+def run():
+    min = int(minimum.get())
+    max = int(maximum.get())
+    valSize = int(size.get())
+    output.set("Returned Value: " + str(eulerGuess(min, max, valSize)))
 
-
+#Label for description and video going more in depth by Vsauce
 description = Label(window, text = "This is a program that simulates the Euler process for guessing the highest number in a series of hidden random numbers.", font = default_font)
 link = Label(window,fg="blue", text = "Link to Vsauce video", cursor = "hand2", font = default_font)
-returnedVal=Label( window,  textvariable = output)
 
-link.bind("<Button-1>", callback)
 description.grid(column=0, row=0)
 link.grid(column=0, row =1)
-returnedVal.grid(column= 0, row = 2)
+link.bind("<Button-1>", callback)
 
+#Text boxes to enter data with accomponying labels
 minimum = Entry(window, width=50)
 minLbl = Label(window, text="Minimum: ", font = default_font)
 maximum = Entry(window, width=50)
@@ -33,18 +41,17 @@ maxLbl = Label(window, text="Maximum: ", font = default_font)
 size = Entry(window, width=50)
 sizeLbl = Label(window, text="Size: ", font = default_font)
 
-minimum.grid(column=1, row = 3)
-minLbl.grid(column=0, row = 3)
-maximum.grid(column=1, row = 4)
-maxLbl.grid(column=0, row = 4)
-size.grid(column=1, row = 5)
-sizeLbl.grid(column=0, row = 5)
 
-def run():
-    min = int(minimum.get())
-    max = int(maximum.get())
-    valSize = int(size.get())
-    output.set("Returned Value: " + str(eulerGuess(min, max, valSize)))
+minimum.grid(column=0, row = 4)
+minLbl.grid(column=0, row = 3)
+maximum.grid(column=0, row = 6)
+maxLbl.grid(column=0, row = 5)
+size.grid(column=0, row = 8)
+sizeLbl.grid(column=0, row = 7)
+
+#Label to show returned value and button to initiate program
+returnedVal=Label( window,  textvariable = output)
+returnedVal.grid(column= 0, row = 2)
 
 start = Button(window, text = "Start", font = default_font, command = run)
 start.grid(column=0, row = 10)
--- simulation.py
+++ simulation.py
@@ -2,14 +2,15 @@ import random
 
 def generate(min, max, size):
     values = []
-    for i in range(0, size+1):
-        values.append(random.randint(min, max+1))
+    for i in range(0, size):
+        values.append(random.randint(min, max))
     return values 
     
 def eulerGuess(min, max, size):
     #min value, max value, size of array to select from
     values = generate(min, max, size)
-    guess = values[4] 
+    guess = values[0] 
+    #TODO Implement actual algorithm lol
     return guess
 
 
