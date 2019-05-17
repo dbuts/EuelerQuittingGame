85c202215fefb47ccd97fd63b951da0212003d0f
David Butler <dbutler4@hawk.iit.edu>
2019-05-17 10:13:18 -0500

    Fixed issue, algorithm now correctly works and is displayed properly

--- simulation.py
+++ simulation.py
@@ -16,20 +16,17 @@ class Simulation:
             self.values.append(random.randint(self.min, self.max))
 
     def eulerGuess(self):
-        #Min value, max value, size of array to select from
-        print(self.values)
         #The stopping point provided by theorem
         stopPoint = ceil(self.size/e)
-        print("Stopping point: "+ str(stopPoint))
         currMax = self.values[0] 
         #Find largest value in fist size/e values
         for x in range(1, stopPoint):
-            if x > currMax:
-                currMax = x
+            if self.values[x] > currMax:
+                currMax = self.values[x]
         #Find and return first value larger than previous max
         for y in range(stopPoint, self.size):
-            if y > currMax:
-                return y
+            if self.values[y] > currMax:
+                return self.values[y]
         #Because we're not allowed to pick a value we revealed and passed on, even if the last value isn't the largest it's the only one we're allowed to pick
         return self.values[self.size-1]
 
